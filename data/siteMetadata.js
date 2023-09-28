d6745d2e8f62b2bafb9a7b5d53a0b0b32e1f9f8b
hlog-kr <hlog.kr@gmail.com>
2023-09-28 10:30:34 +0900

    Modify siteMetadata

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,9 +1,9 @@
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
-  language: 'en-us',
+  title: 'HLog Next.js - for git',
+  author: 'hlog',
+  headerTitle: 'HLog Mirroring',
+  description: 'HLog 블로그의 Next.js 글 이해를 돕기 위한 Git 소스 공유용 블로그입니다.',
+  language: 'ko-KR',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
   siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
@@ -11,12 +11,12 @@ const siteMetadata = {
   image: '/static/images/avatar.png',
   socialBanner: '/static/images/twitter-card.png',
   email: 'address@yoursite.com',
-  github: 'https://github.com',
-  twitter: 'https://twitter.com/Twitter',
-  facebook: 'https://facebook.com',
-  youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com',
-  locale: 'en-US',
+  github: 'https://github.com/hlog-kr',
+  twitter: '',
+  facebook: '',
+  youtube: '',
+  linkedin: '',
+  locale: 'ko-KR',
   analytics: {
     // If you want to use an analytics provider you have to add it to the
     // content security policy in the `next.config.js` file.
